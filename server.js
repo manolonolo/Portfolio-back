78b3f633d1a385a62a5b6def84bcd0df9d50c88a
Manolo <p.manolaki95@gmail.com>
2023-01-13 10:14:09 -0300

    updated port

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const port = process.env.PORT || 5000;
 
 // server used to send send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.listen(port, () => console.log(`Server Running in port ${port}`));
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
